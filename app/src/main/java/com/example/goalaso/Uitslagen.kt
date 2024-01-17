c61f6fd918411ca1e113c13d61829010bb3680c9
133411 <1313wtr4ewt@kjjl>
2024-01-17 14:16:01 +0100

    achtergrond en logo aangepast

--- app/src/main/java/com/example/goalaso/Uitslagen.kt
+++ app/src/main/java/com/example/goalaso/Uitslagen.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.Navigation
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -15,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [landing.newInstance] factory method to
+ * Use the [Uitslagen.newInstance] factory method to
  * create an instance of this fragment.
  */
-class landing : Fragment() {
+class Uitslagen : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,17 +32,9 @@ class landing : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_landing, container, false)
-        view.findViewById<Button>(R.id.btnUitslagen).setOnClickListener {
-            //Use the navigation tree in the current view to navigate to the next page
-            Navigation.findNavController(view).navigate(R.id.action_landing_to_uitslagen)
-
-
-        }
-        return view;
+        return inflater.inflate(R.layout.fragment_uitslagen, container, false)
     }
 
     companion object {
@@ -54,12 +44,12 @@ class landing : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment landing.
+         * @return A new instance of fragment Uitslagen.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            landing().apply {
+            Uitslagen().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
