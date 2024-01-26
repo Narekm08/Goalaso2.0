a82913d7712386996bc63dd9541450e61b79e26e
133411 <dqewdqwe@gmail.com>
2024-01-26 12:08:45 +0100

    button gefixt

--- app/src/main/java/com/example/goalaso/landing.kt
+++ app/src/main/java/com/example/goalaso/landing.kt
@@ -44,6 +44,13 @@ class landing : Fragment() {
 
 
         }
+
+        view.findViewById<Button>(R.id.btnStanden).setOnClickListener {
+            //Use the navigation tree in the current view to navigate to the next page
+            Navigation.findNavController(view).navigate(R.id.action_landing_to_standen)
+
+
+        }
         return view;
     }
 
