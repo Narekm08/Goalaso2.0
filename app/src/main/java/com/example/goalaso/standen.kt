39e0971e20dced187b68d584b32fdbca6fffe4ac
133613 <dnjdkdj@cncljd>
2024-02-02 12:28:35 +0100

    eredivisie standen in gevuld.

--- app/src/main/java/com/example/goalaso/standen.kt
+++ app/src/main/java/com/example/goalaso/standen.kt
@@ -37,46 +37,31 @@ class standen : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_standen, container, false)
+
         view.findViewById<Button>(R.id.btneredivisie).setOnClickListener {
-            view.findViewById<TextView>(R.id.textView2).text = "PSV "
-            view.findViewById<TextView>(R.id.textView4).text = "Feyenoord "
-            view.findViewById<TextView>(R.id.textView5).text = "Twente "
-            view.findViewById<TextView>(R.id.textView6).text = "AZ"
-            view.findViewById<TextView>(R.id.textView7).text = "Ajax"
-            view.findViewById<TextView>(R.id.textView8).text = "GA Eagles"
-            view.findViewById<TextView>(R.id.textView9).text = "NEC"
-            view.findViewById<TextView>(R.id.textView10).text = "Sparta Rotterdam"
-            view.findViewById<TextView>(R.id.textView11).text = "PEC"
-            view.findViewById<TextView>(R.id.textView12).text = "Fortuna"
-            view.findViewById<TextView>(R.id.textView13).text = "Excelsior"
-            view.findViewById<TextView>(R.id.textView14).text = "Heerenveen"
-            view.findViewById<TextView>(R.id.textView15).text = "Utrecht"
-            view.findViewById<TextView>(R.id.textView16).text = "Almere"
-            view.findViewById<TextView>(R.id.textView17).text = "Heracles"
-            view.findViewById<TextView>(R.id.textView18).text = "Rkc"
-            view.findViewById<TextView>(R.id.textView19).text = "Volendam"
-            view.findViewById<TextView>(R.id.textView20).text = "Vitesse"
+            view.findViewById<TextView>(R.id.textView5).text = " Clubs                                              G    W    G    V    DV    DT    DS    Pts"
+            view.findViewById<TextView>(R.id.textView6).text = "1 PSV                                               19  18    1    0    62    8     54    55"
+            view.findViewById<TextView>(R.id.textView7).text = "2 Feyenoor                                          19  13    4    2    52   17     35    43"
+            view.findViewById<TextView>(R.id.textView8).text = "3 Twente                                            19  11    5    3    36   21     15    38"
+            view.findViewById<TextView>(R.id.textView9).text = "4 AZ                                                19  10    5    4    40   20     20    35               "
+            view.findViewById<TextView>(R.id.textView10).text = "5 Ajax                                             19"
+            view.findViewById<TextView>(R.id.textView11).text = "6 GA Eagles"
+            view.findViewById<TextView>(R.id.textView12).text = "7 NEC"
+            view.findViewById<TextView>(R.id.textView13).text = "8 Sparta Rotterdam"
+            view.findViewById<TextView>(R.id.textView14).text = "9 PEC"
+            view.findViewById<TextView>(R.id.textView15).text = "10 Fortuna"
+            view.findViewById<TextView>(R.id.textView16).text = "11 Excelior"
+            view.findViewById<TextView>(R.id.textView17).text = "12 Heerenveen"
+            view.findViewById<TextView>(R.id.textView18).text = "13 Utrecht"
+            view.findViewById<TextView>(R.id.textView19).text = "14 Almere"
+            view.findViewById<TextView>(R.id.textView20).text = "15 Heracles"
+            view.findViewById<TextView>(R.id.textView3).text = "16 RKC"
+            view.findViewById<TextView>(R.id.textView21).text = "17 volendam"
+            view.findViewById<TextView>(R.id.textView22).text = "18 Vitesse"
         }
+
         return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment standen.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            standen().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
