bac2693539c464e633e8cfae02fd8ced1f58e964
133613 <dnjdkdj@cncljd>
2024-02-28 15:12:31 +0100

    ik heb de helft van de api toegevoegd aan mijn appe

---
+++ app/src/main/java/com/example/goalaso/Standings.java
@@ -0,0 +1,48 @@
+package com.example.goalaso;
+
+public class Standings {
+    private String get;
+    Parameters ParametersObject;
+    ArrayList < Object > errors = new ArrayList < Object > ();
+    private float results;
+    Paging PagingObject;
+    ArrayList < Object > response = new ArrayList < Object > ();
+
+
+// Getter Methods
+
+    public String getGet() {
+        return get;
+    }
+
+    public Parameters getParameters() {
+        return ParametersObject;
+    }
+
+    public float getResults() {
+        return results;
+    }
+
+    public Paging getPaging() {
+        return PagingObject;
+    }
+
+// Setter Methods
+
+    public void setGet(String get) {
+        this.get = get;
+    }
+
+    public void setParameters(Parameters parametersObject) {
+        this.ParametersObject = parametersObject;
+    }
+
+    public void setResults(float results) {
+        this.results = results;
+    }
+
+    public void setPaging(Paging pagingObject) {
+        this.PagingObject = pagingObject;
+    }
+}
+
